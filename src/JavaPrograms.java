a7c00e16085cee855d79a52cd04ce35a026890b0
Prasad <prasad26.te@gmail.com>
2024-06-29 10:36:43 +0530

    Reverse Number Program

--- src/JavaPrograms.java
+++ src/JavaPrograms.java
@@ -2,8 +2,18 @@
 public class JavaPrograms {
 
 	public static void main(String[] args) {
+		reverseNumber(123456789);
 
-
+	}
+	
+	public static void reverseNumber(int number) {
+		int revNum=0, newNum=0;
+		
+		while(number%10>0) {
+			revNum=revNum*10+number%10;
+			number=number/10;
+		}
+		System.out.println(revNum);
 	}
 
 }
