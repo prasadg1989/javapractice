433784a88eaf923b05cc8ff8583d584b22b1d1fa
Prasad <prasad26.te@gmail.com>
2024-07-02 17:39:53 +0530

    Count of Numbers

--- src/JavaPrograms.java
+++ src/JavaPrograms.java
@@ -1,9 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class JavaPrograms {
 
 	public static void main(String[] args) {
 		reverseNumber(123456789);
-
+		numbersCount(1232694891);
 	}
 	
 	public static void reverseNumber(int number) {
@@ -16,4 +18,18 @@ public class JavaPrograms {
 		System.out.println(revNum);
 	}
 
+	public static void numbersCount(long number) {
+		char[] charArr=Long.toString(number).toCharArray();
+		Map<Character, Integer> charCount=new HashMap<Character, Integer>();
+		for(int i=0; i<charArr.length;i++) {
+			if(charCount.containsKey(charArr[i])) {
+				charCount.put(charArr[i], charCount.get(charArr[i])+1);
+			}else {
+				charCount.put(charArr[i], 1);
+			}
+		}
+		System.out.println(charCount);
+	}
+	
+	
 }
